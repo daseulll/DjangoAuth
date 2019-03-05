014afa6fdbb1b45629375f4bff99a3e2914303c0
daseulll <nldaseul@gmail.com>
2019-03-05 21:47:49 +0900

    Add accounts app - signup, login, logout feature

--- djangoauth/urls.py
+++ djangoauth/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('', lambda req: redirect('/blog/')), # URL Reverse
+    path('', lambda req: redirect('blog:post_list'), name='root'), # URL Reverse
 ]
