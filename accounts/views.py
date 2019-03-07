00a6409709a24f70e5282935e55a68c485b397bf
daseulll <nldaseul@gmail.com>
2019-03-07 10:01:23 +0900

    Add resolve_url for user conveinience added get_success_url and form_vaild function

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from .forms import SignupForm
 
 # def signup(request):
@@ -11,19 +12,32 @@ from .forms import SignupForm
 #         form = SignupForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             # 회원 생성됐으므로, 후 처리(자동로그인 등)
+#             auth_login(request, user)
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
 #         'form' : form,
 #     })
 
-signup = CreateView.as_view(
-    model= User, 
-    form_class = SignupForm,
-    success_url = settings.LOGIN_URL,
-    template_name = 'accounts/signup.html',
-    )
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user) 
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
+
 
 @login_required
 def profile(request):
