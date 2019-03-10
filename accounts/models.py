b5d6f59c05fac985fb05debcdcc4fa9839a11866
daseulll <nldaseul@gmail.com>
2019-03-10 10:27:05 +0900

    Add User session model and apply migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager as AuthUserManager
+from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save 
 from django.core.mail import send_mail
 from django.db import models
@@ -41,4 +42,10 @@ def on_post_save_for_user(sender, **kwargs):
             fail_silently=False,
         )
 
-post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL) 
+post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
+
+
+class UserSession(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, editable=False)
+	session_key = models.CharField(max_length=40, editable=False)
+	created_at = models.DateTimeField(auto_now_add=True)
