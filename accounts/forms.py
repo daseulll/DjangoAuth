92373a4a083d0f4106d8c289e33e5309f419f814
daseulll <nldaseul@gmail.com>
2019-03-10 13:49:56 +0900

    fix: Override model=User of class Meta in SignupForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import validate_email
+from .models import User
 
 
 class SignupForm(UserCreationForm):
-    User = get_user_model()
     # 유효성검사: 각 필드에 대한 validators, clean_필드명, clean 로 수행
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) #super()로 부모데이터를 호출하면, self.fields에 dict형태로 부모의 필드 데이터가 모두 담김
@@ -20,6 +20,10 @@ class SignupForm(UserCreationForm):
             user.save()
         
         return user
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # fields ...
 
 
     # def clean_username(self):
