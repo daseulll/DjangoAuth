d2b6f4693815f223fc23cb42aefdb4f1dba928a6
daseulll <nldaseul@gmail.com>
2019-03-11 16:56:52 +0900

    fix: Create kickMiddleware to imporove a duplicate login issue

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,3 @@
-from importlib import import_module
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager as AuthUserManager
@@ -6,8 +5,6 @@ from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save 
 from django.core.mail import send_mail
 from django.db import models
-
-SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 class UserManager(AuthUserManager):
     def create_superuser(self, username, email, password, **extra_fields):
@@ -56,15 +53,6 @@ class UserSession(models.Model):
 def kick_my_other_sessions(sender, request, user, **kwargs):
     print('kick my other sessions')
 
-    for user_session in UserSession.objects.filter(user=user):
-        session_key = user_session.session_key
-        session = SessionStore(session_key) 
-        # session.delete()
-        session['kicked'] = True
-        session.save()
-        user_session.delete()
-
-    session_key = request.session.session_key
-    UserSession.objects.create(user=user, session_key=session_key)
+    user.is_user_logged_in = True
 
 user_logged_in.connect(kick_my_other_sessions)
